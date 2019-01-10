6845e90febb1c4755a1bf720a1d125d768cb68aa
Higgs <higgs@weidong.local>
2019-01-10 15:11:10 +0800

    commit

--- quora_insincere/run_classifier.py
+++ quora_insincere/run_classifier.py
@@ -418,7 +418,7 @@ class QuoraTitleProcessor(DataProcessor):
     def get_dev_examples(self, data_dir):
         return self._create_examples(os.path.join(data_dir, 'test_s.csv'), 'dev')
 
-    def get_dev_examples(self, data_dir):
+    def get_test_examples(self, data_dir):
         return self._create_examples(os.path.join(data_dir, 'test.csv'), 'test')
 
     def get_labels(self):
